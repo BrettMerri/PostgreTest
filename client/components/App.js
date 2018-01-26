c6c24602a87148b5ecbad579574d3e2c19a9d7f8
Brett Merrifield <the.b.merri@gmail.com>
2018-01-25 22:07:05 -0500

    initial commit

---
+++ client/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <p>App</p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ client/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import 'jquery';
+import 'bootstrap/dist/js/bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/main.scss';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app'),
+);
