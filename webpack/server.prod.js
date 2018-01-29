91a5f22b3627b8dcc6c12139e43056a82f1d843c
Brett Merrifield <the.b.merri@gmail.com>
2018-01-28 19:45:09 -0500

    plugin

--- webpack/server.prod.js
+++ webpack/server.prod.js
@@ -42,11 +42,11 @@ module.exports = {
     ],
   },
   plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
-    }),
+    // new webpack.DefinePlugin({
+    //   'process.env': {
+    //     'NODE_ENV': JSON.stringify('production')
+    //   }
+    // }),
     new UglifyJSPlugin({
       sourceMap: true
     }),
